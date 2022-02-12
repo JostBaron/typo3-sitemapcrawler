5c83701e822a6d3be6bbf0faa565fcb11cf66892
Jost Baron <j.baron@netzkoenig.de>
2022-02-12 19:23:59 +0100

    [BUGFIX] Move table registration to ext_tables.php.

--- Configuration/TCA/tx_jbaron_sitemapcrawler_domain_model_sitemapurl.php
+++ Configuration/TCA/tx_jbaron_sitemapcrawler_domain_model_sitemapurl.php
@@ -6,9 +6,6 @@
  * file 'LICENSE.txt', which is part of this source code package.
  */
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
-    'tx_jbaron_sitemapcrawler_domain_model_sitemapurl'
-);
 $GLOBALS['TCA']['tx_jbaron_sitemapcrawler_domain_model_sitemapurl'] = [
     'ctrl' => [
         'title'             => 'Sitemap URL',
---
+++ ext_tables.php
@@ -0,0 +1,5 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages(
+    'tx_jbaron_sitemapcrawler_domain_model_sitemapurl'
+);
